daeedfac6cdb30a4197561b2cc85a2752a65d96b
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-03 22:00:35 -0300

    switchmode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SwitchMode from './components/SwitchMode';
 import './App.css';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const handleChangeMode = () => {
+    if(mode === 'light')
+      setMode('dark');
+    else
+      setMode('light');
+  }
   return (
-    <div className="App">
+    <div className={mode}>
       <h1>Programacionok</h1>
+      <SwitchMode setMode={handleChangeMode} />
     </div>
   );
 }
