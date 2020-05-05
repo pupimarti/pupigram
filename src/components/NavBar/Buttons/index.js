88103ba91abee08d351e4f61a82ec46669ccbf0a
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-05 13:22:33 -0300

    Bar ok

--- src/components/NavBar/Buttons/index.js
+++ src/components/NavBar/Buttons/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import home from './home.svg';
+import heart from './heart.svg';
+import brain from './brain.svg';
+import smile from './smileys.svg';
 import './css.css';
 
 export default function Buttons() {
     return (
         <div className="content-buttons">
             <img className="icon" src={home} alt="Home" />
+            <img className="icon" src={brain} alt="Frases" />
+            <img className="icon" src={smile} alt="Memes" />
+            <img className="icon" src={heart} alt="Notif" />
         </div>
     )
 }
