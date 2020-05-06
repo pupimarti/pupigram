3c8559ea05b6390aa204e6163a7edd0536691c1a
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-05 22:51:37 -0300

    like post img

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './css.css';
 import account from './account.svg';
 import comment from './chat.svg';
 import Comment from './Comment';
 import Like from './Like';
+import heart from './Like/heart-selected.svg';
 
 export default function Post(props) {
+
+    const [likeImg, setlikeImg] = useState(false)
+    const handleClickLikeImg = () => {
+        setlikeImg(!likeImg);
+    }
 
     return (
         <div className="content-post">
@@ -20,7 +26,10 @@ export default function Post(props) {
                     <div className="point"></div>
                 </div>
             </div>
-            <img onDoubleClick={() => {alert('like')}} className="img-post" src="https://instagram.faep9-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/85167617_661877847898209_1139547952866115949_n.jpg?_nc_ht=instagram.faep9-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=T5rDv6fOedIAX87rTx7&oh=3fc18a7535d6ce74e93619e0c5d39667&oe=5EDB30BA" alt="postimg"/>
+            <div className="content-img-post">
+                <img className={likeImg ? "like-img-post animation-like" :"like-img-post"} src={heart} alt="corazon" />
+                <img onDoubleClick={handleClickLikeImg} className="img-post" src="https://instagram.faep9-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/85167617_661877847898209_1139547952866115949_n.jpg?_nc_ht=instagram.faep9-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=T5rDv6fOedIAX87rTx7&oh=3fc18a7535d6ce74e93619e0c5d39667&oe=5EDB30BA" alt="postimg"/>
+            </div>
             <div className="center-post actions-post">
                 <Like />
                 <img className="action" src={comment} alt="comment"/>
