320f274a084b0a1d54cef078202bf0da1cbfe0c9
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-09 18:23:31 -0300

    Updates

---
+++ precache-manifest.2c62f23ae1c97b734add6b696aa99cd8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d23f9e5da6f937d93763e57f9b2f463",
+    "url": "/pupigram/index.html"
+  },
+  {
+    "revision": "5c7cea676adaef6bd0d0",
+    "url": "/pupigram/static/css/main.dc99731a.chunk.css"
+  },
+  {
+    "revision": "2e2d7e381fb4d2e16539",
+    "url": "/pupigram/static/js/2.84ba40df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pupigram/static/js/2.84ba40df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7cea676adaef6bd0d0",
+    "url": "/pupigram/static/js/main.bfc00537.chunk.js"
+  },
+  {
+    "revision": "abf3d0833c4634d33f39",
+    "url": "/pupigram/static/js/runtime-main.1689c465.js"
+  },
+  {
+    "revision": "71672f0b63989fd7780aa2fbc4f57ad3",
+    "url": "/pupigram/static/media/account.71672f0b.svg"
+  },
+  {
+    "revision": "91318a8fa1ea4ab2e1cd24ac26407dfc",
+    "url": "/pupigram/static/media/account.91318a8f.svg"
+  },
+  {
+    "revision": "c817e915c9a4d090bf7c20e346f1b905",
+    "url": "/pupigram/static/media/add.c817e915.svg"
+  },
+  {
+    "revision": "c6601c4410516337d9a347a161dddd00",
+    "url": "/pupigram/static/media/chat.c6601c44.svg"
+  },
+  {
+    "revision": "c26fe294ca099acdf04db2344c70a3af",
+    "url": "/pupigram/static/media/direct.c26fe294.svg"
+  },
+  {
+    "revision": "f2610513c5a854fe4600d64db50aebd7",
+    "url": "/pupigram/static/media/heart-selected.f2610513.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/pupigram/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "8b41ca2e4953bb7e8cc248acee31994a",
+    "url": "/pupigram/static/media/home-select.8b41ca2e.svg"
+  },
+  {
+    "revision": "6f087f95ffc4946f72d8d4e7513ca13e",
+    "url": "/pupigram/static/media/home.6f087f95.svg"
+  },
+  {
+    "revision": "c884f427a7f5b780545f8a264cdc8688",
+    "url": "/pupigram/static/media/verify.c884f427.svg"
+  }
+]);
