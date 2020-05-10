da6788cc5d62cb5dc818d65c4f0da54c05399dd9
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-10 13:35:53 -0300

    public posts button

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import verify from "../../img/verify.svg";
+import posts from "./posts.svg";
 import "./css.css";
 
 export default function User() {
@@ -24,17 +25,80 @@ export default function User() {
             </div>
           </div>
           <div className="info-user stats">
-              <p className="stats-user"><b>585</b> publicaciones</p>
-              <p className="stats-user"><b>150mmm</b> seguidores</p>
-              <p className="stats-user"><b>229</b> seguidos</p>
+            <p className="stats-user">
+              <b>585</b> publicaciones
+            </p>
+            <p className="stats-user">
+              <b>150mmm</b> seguidores
+            </p>
+            <p className="stats-user">
+              <b>229</b> seguidos
+            </p>
           </div>
-        <div>
+          <div>
             <h4 className="username-name">Leo Messi</h4>
-            <p className="description-user">Bienvenidos a la cuenta oficial de Instagram de Leo Messi / Welcome to the official Leo Messi Instagram account</p>
-            <a className="web-user" href="www.messi.com">messi.com</a>
+            <p className="description-user">
+              Bienvenidos a la cuenta oficial de Instagram de Leo Messi /
+              Welcome to the official Leo Messi Instagram account
+            </p>
+            <a className="web-user" href="www.messi.com">
+              messi.com
+            </a>
           </div>
         </div>
       </header>
+      <section className="posts-user">
+        <div className="posts-user-public">
+          <div className="content-public">
+            <img className="icon" src={posts} alt="Publicaciones" />
+            <p className="pc">PUBLICACIONES</p>
+          </div>
+        </div>
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+        <img
+          className="post-user"
+          src="https://instagram.faep9-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/96144021_262013868284125_5495302340245040052_n.jpg?_nc_ht=instagram.faep9-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Z5B4GNO9PK4AX9AFq5t&oh=e374397f83f37a3fd0eee550073dbce9&oe=5EE1E98A"
+          alt="post"
+        />
+      </section>
     </div>
   );
 }
