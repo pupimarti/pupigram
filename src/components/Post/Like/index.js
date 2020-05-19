402f4d2922a37a185a59a5db1e586c606c28e305
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-19 10:24:16 -0300

    optimize like

--- src/components/Post/Like/index.js
+++ src/components/Post/Like/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import './css.css';
-import heart from '../../../img/heart.svg';
-import heartSelected from '../../../img/heart-selected.svg';
+import React, {useState, useEffect} from "react";
+import "./css.css";
+import heart from "img/heart.svg";
+import heartSelected from "img/heart-selected.svg";
 
 export default function Like(props) {
-    
-    if(props.like === 'true') return <img className="action like-selected" onClick={() => props.setlike(false)} src={heartSelected} alt="heart"/>
-    
-    if(props.like === 'false') return <img className="action" onClick={() => props.setlike(true)} src={heart} alt="heart"/>
 
-    if(props.like) return <img className="like action like-selected" onClick={() => props.setlike(false)} src={heartSelected} alt="heart"/>
+    const [animation, setAnimation] = useState(false);
 
-    return <img className={"action dislike"} onClick={() => props.setlike(true)} src={heart} alt="heart"/>
+    useEffect(() => {
+        const hasAnimate = () => {
+            if(props.like === 'true' || props.like === 'false')
+                return false;
+            return true;
+        }
+        setAnimation(hasAnimate());
+    }, [props])
 
+
+    const isLike = () => {
+        if(props.like === 'true' || props.like === true)
+            return true;
+        return false;
+    }
+
+    const handleClickLike = () => {
+        if(isLike())
+            props.setlike(false);
+        else
+            props.setlike(true);
+    }
+
+    const getClassName = () => {
+        var className = "action";
+        if(isLike())
+            className += " like-selected"
+        if(animation)
+            className += " like-animation"
+        return className;
+    }
+
+    return(
+        <img
+            className={getClassName()}
+            onClick={handleClickLike}
+            src={isLike() ? heartSelected : heart}
+            onAnimationEnd={() => setAnimation(false)}
+            alt="heart"
+        />
+    )
 }
