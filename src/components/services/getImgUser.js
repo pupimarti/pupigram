51efb5ca29868fe9b87789d4e92869a6bf9748ff
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-06-03 11:51:05 -0300

    img-users save localstorage

--- src/components/services/getImgUser.js
+++ src/components/services/getImgUser.js
@@ -2,16 +2,32 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 export default async function getImgUser(user) {
-    const storage = firebase.storage();
     let return_img = null;
-    await storage.ref('users/'+user).getDownloadURL()
-    .then(function(url) {
-        return_img = url;
-    })
-    .catch(function(error) {
-        console.log("No se pudo encontrar la imagen de perfil de ", user);
-        console.log(error);
-        return_img = null;
-    });
+    const storage_users = localStorage.getItem('img_users');
+    let img_users = JSON.parse(storage_users);
+    if(img_users && img_users.forEach((u) => {
+        if(u.user === user){
+            return_img = u.url;
+            return;
+        }
+    }));
+    if(!return_img){
+        console.log('vine a buscar img');
+        const storage = firebase.storage();
+        await storage.ref('users/'+user).getDownloadURL()
+        .then(function(url) {
+            const user_img = {user,url};
+            if(!img_users)
+                img_users = [];
+            img_users.push(user_img);
+            localStorage.setItem('img_users', JSON.stringify(img_users));
+            return_img = url;
+        })
+        .catch(function(error) {
+            console.log("No se pudo encontrar la imagen de perfil de ", user);
+            console.log(error);
+            return_img = null;
+        });
+    }
     return return_img;
 }
