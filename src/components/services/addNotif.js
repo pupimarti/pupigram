084c51f020ccc0cfc18fd4cbd6ed752ea8e78963
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-06-11 12:07:09 -0300

    fix no notif auto-like auto-comment

--- src/components/services/addNotif.js
+++ src/components/services/addNotif.js
@@ -2,6 +2,7 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 export default function addNotif(user, type, user_send, post = null, comment = null) {
+    if(user === user_send) return;
     const db = firebase.firestore();
     let result = null;
     let notif = {
